235b566548c5994c7bcfe29904d11f5277528e8d
Anil bhau <anil@gmail.com>
2025-10-01 00:35:13 +0530

    AB | terraform-code-added

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,12 +6,33 @@ provider "aws" {
 
 
 resource "aws_instance" "ec2" {
-    ami= "ami-0ca4d5db4872d0c28"
-    instance_type = "t2.micro"
-    key_name = "athrav-ohio"
+    ami= var.image_id
+    instance_type = var.instance_type
+    key_name = var.key_pair
     security_groups = [ "default" ]
   
    tags = {
      Name = "my-server"
    }
+}
+
+variable image_id {
+  type        = string
+  default     = "ami-0ca4d5db4872d0c28"
+  description = "ENTER AMI ID"
+}
+
+
+variable instance_type {
+  type = string
+  default = "t2.micro"
+}
+
+variable key_pair {
+  type = string
+  default = "thrav-ohio"
+}
+
+output public_ip {
+  value       = aws_instance.ec2.public_ip
 }
