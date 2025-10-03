2677f510ae3872e24f34cc2a803f892980e88fd1
Anil bhau <anil@gmail.com>
2025-10-03 10:19:10 +0530

    tf-code-added

---
+++ terraform/ec2-alb-as/var.tf
@@ -0,0 +1,23 @@
+variable "image_id" {
+    default = "ami-0f88e80871fd81e91"
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "key_pair" {
+    default = "vergenia-adi-key"
+}
+
+variable "subnet_id_1" {
+    default = "subnet-09b9967ebb94e39b9"
+}
+
+variable "subnet_id_2" {
+    default = "subnet-05ffa676a7daaf1f0"
+}
+
+variable "vpc_id" {
+    default = "vpc-08c2dcc23b9cb116c"
+}
